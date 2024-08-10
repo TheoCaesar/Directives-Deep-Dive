b3354b56ba97d22c443a9f0f3dd2180e3324abd6
TheoCaesar <theocaesar7@gmail.com>
2024-08-10 07:01:17 +0100

    host binding - get host props bound to directive class props

--- src/app/directives/render-highlighter.directive.ts
+++ src/app/directives/render-highlighter.directive.ts
@@ -1,18 +1,22 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { style } from '@angular/animations';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appRenderHighlighter]' // attr selector
 })
 export class RenderHighlighterDirective {
+  @HostBinding("style.backgroundColor") bgColor = "transparent"
   constructor(private renderer:Renderer2, private elementRef: ElementRef) { }
   ngOnInit() {
     // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "aqua" )
   }
   @HostListener('mouseenter') onMouseOver(eventMetaData: Event){
-    this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "aqua" )
+    // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "aqua" )
+    this.bgColor = 'aqua';
   }
 
   @HostListener('mouseleave') onMouseLeave(eventMetaData: Event){
-    this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "white" )
+    // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "white" )
+    this.bgColor = 'transparent';
   }
 }
