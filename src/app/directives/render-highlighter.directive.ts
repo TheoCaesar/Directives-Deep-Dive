df65a6c6102dbde9c1baaf21b29de641dce95574
TheoCaesar <theocaesar7@gmail.com>
2024-08-10 06:28:10 +0100

    hostlistener - custom events for attribute directives

--- src/app/directives/render-highlighter.directive.ts
+++ src/app/directives/render-highlighter.directive.ts
@@ -1,12 +1,18 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appRenderHighlighter]' // attr selector
 })
 export class RenderHighlighterDirective {
-
   constructor(private renderer:Renderer2, private elementRef: ElementRef) { }
   ngOnInit() {
+    // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "aqua" )
+  }
+  @HostListener('mouseenter') onMouseOver(eventMetaData: Event){
     this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "aqua" )
+  }
+
+  @HostListener('mouseleave') onMouseLeave(eventMetaData: Event){
+    this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "white" )
   }
 }
